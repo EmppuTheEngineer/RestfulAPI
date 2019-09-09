1d5631860a05e97fa327e10173ae42a8aa204067
Emppu Nurminen <emilia.nurminen@siili.com>
2019-09-09 11:58:03 +0300

    Reads and prints in logs the requested path url

--- app/index.js
+++ app/index.js
@@ -7,14 +7,17 @@
 
  // Dependencies
  const http = require('http');
+ const url = require('url');
 
  // Defining simple HTTP server
  const httpServer = http.createServer(function(req, res){
+    let parsedUrl = url.parse(req.url, true);
+    let path = parsedUrl.pathname;
     res.end('Yaay it\'s working\n');
+    console.log('Request recieved on path: ' + path);
  });
 
  // Start the server and listen port 3000
  httpServer.listen(3000, function(){
     console.log('Server is now listening on port 3000');
  });
-
