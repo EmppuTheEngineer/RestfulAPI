674bb2ea7c9f50120704dd6e2214c4af7c41b242
Emppu Nurminen <emilia.nurminen@siili.com>
2019-09-09 13:44:50 +0300

    Adds the router and handlers to route both /hello and notFound correctly

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 /*
  * Homework Assignment #1 for Node Masterclass by Thinkific
  * Author:  Emppu Nurminen
+ * Date:    9th of September, 2019
  * Detail:  Returns simple welcome text in JSON form,
  *          when user selects localhost:3000/hello
  */
@@ -12,20 +13,57 @@
  // Defining simple HTTP server
  const httpServer = http.createServer(function(req, res){
 
-    //Get the URL object
+    //Geting the URL object
     let parsedUrl = url.parse(req.url, true);
 
-    //Get the path
+    //Geting the path
     let path = parsedUrl.pathname;
 
-    //Send the respond
+    //Geting trimmed path
+    let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Choosing requested handler
+    // If not found, using notFound
+    let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+   
+    // Routing the request to the handgler spefied in the router
+    chosenHandler(function(statusCode, payload){
+       //Setting the content type for JSON
+       res.setHeader('Content-Type', 'application/json');
+       
+       //Setting the statuscode for header
+       res.writeHead(statusCode);
+
+       //Setting the content to be recognized as JSON
+       res.end(JSON.stringify(payload));
+    });
+
+    //Sending the respond
     res.end('Yaay it\'s working\n');
 
-    //Log the recieved path
+    //Logging the recieved path
     console.log('Request recieved on path: ' + path);
  });
 
- // Start the server and listen port 3000
+ // Starting the server and listen port 3000
  httpServer.listen(3000, function(){
     console.log('Server is now listening on port 3000');
  });
+
+ //Defining handlers
+ const handlers = {};
+ 
+ //Hello handler
+ handlers.hello = function(callback){
+   callback(200, {'Hello' : 'world!'})
+ };
+
+ //Not defined handler
+ handlers.notFound = function(callback){
+    callback(404, {'Not' : 'Found'})
+ }
+
+//Defining router
+const router = {
+   'hello' : handlers.hello
+}
